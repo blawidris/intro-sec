8cceb9fb5b96822b0f13bca297c0c7b00630ef08
blawidris <blawidris@gmail.com>
2023-04-22 22:38:56 +0100

    fixed dropdown menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,50 @@
-let dropdownMenu = document.querySelectorAll(".dropdown");
+// dropdown
 
-dropdownMenu.forEach((item) => {
-  item.addEventListener("click", function (event) {
-    event.preventDefault();
+const showMenu = document.querySelectorAll('.dropdown .nav-link');
 
-    let dropdown = document.querySelector(".dropdown-menu");
-    let dropdownIcon = document.querySelector(".dropdown img");
+showMenu.forEach((item) => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
 
-    if (
-      dropdownIcon.getAttribute("src") ===
-      "./assets/images/icon-arrow-down.svg"
-    ) {
-      dropdownIcon.setAttribute(
-        "src",
-        "./assets/images/icon-arrow-up.svg"
-      );
-    } else {
-      dropdownIcon.setAttribute(
-        "src",
-        "./assets/images/icon-arrow-down.svg"
-      );
-    }
+        let itemDropdownIcon = this.firstElementChild;
 
-    dropdown.classList.toggle("show");
-    // console.log(this.firstElementChild())
-  });
-});
+        if (itemDropdownIcon.getAttribute('src') === './assets/images/icon-arrow-down.svg') {
+            itemDropdownIcon.setAttribute('src', './assets/images/icon-arrow-up.svg')
+        } else {
+            itemDropdownIcon.setAttribute('src', './assets/images/icon-arrow-down.svg')
+        }
 
+        let dropdownMenu = this.nextElementSibling;
+
+        //    add add show class
+        dropdownMenu.classList.toggle('show')
+    });
+})
+
+// hamburger
 const showMenuBtn = document.querySelector('.btn-menu');
 const closeMenuBtn = document.querySelector('.btn-icon-x');
 const bodyDOM = document.querySelector('body');
-    const navMenu = document.querySelector('.navbar-container'); 
+const navMenu = document.querySelector('.navbar-container');
 
-showMenuBtn.addEventListener('click', (e)=>{
-   
+showMenuBtn.addEventListener('click', (e) => {
+
     let navItems = document.querySelectorAll('.navbar-nav .nav-item');
 
-    navItems.forEach((item, key)=>{
-      key++;
-      item.classList.add('animate-1')
-      item.style.animationDelay = `${key * 150}ms`
+    navItems.forEach((item, key) => {
+        key++;
+        item.classList.add('animate-1')
+        item.style.animationDelay = `${key * 150}ms`
     })
 
     bodyDOM.classList.add('overlay')
     navMenu.classList.add('show', 'animate-3')
     // navMenu.classList.add('')
-    
+
 })
 
-closeMenuBtn.addEventListener('click', (e)=>{
-  
+closeMenuBtn.addEventListener('click', (e) => {
+
     bodyDOM.classList.remove('overlay')
     navMenu.classList.remove('show')
 })
